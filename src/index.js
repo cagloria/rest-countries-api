5c2f3467f0649a0b3bb37e249c7c4d32ce70d39b
cagloria <33134245+cagloria@users.noreply.github.com>
2020-07-22 14:01:35 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+    return <h1>REST Countries API</h1>;
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
