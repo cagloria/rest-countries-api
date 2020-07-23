b77632471d7a1adc3378cc6bbddea45e3768a3ed
cagloria <33134245+cagloria@users.noreply.github.com>
2020-07-22 17:23:46 -0700

    Set region and capital to display as N/A if empty
    - If a region or capital is empty, displays as N/A
    - Fixed alt description of country flag

--- src/components/Country.js
+++ src/components/Country.js
@@ -1,23 +1,20 @@
 import React from "react";
 
 function Country({ obj }) {
+    const { flag, name, population, region, capital } = obj;
+
     return (
         <li>
-            <img
-                src={obj.flag}
-                alt={`Flag of ${obj.flag}`}
-                width="300"
-                height="200"
-            />
-            <h2>{obj.name}</h2>
+            <img src={flag} alt={`Flag of ${name}`} width="300" height="200" />
+            <h2>{name}</h2>
             <p>
-                <strong>Population:</strong> {obj.population.toLocaleString()}
+                <strong>Population:</strong> {population.toLocaleString()}
             </p>
             <p>
-                <strong>Region:</strong> {obj.region}
+                <strong>Region:</strong> {region.length > 0 ? region : "N/A"}
             </p>
             <p>
-                <strong>Capital:</strong> {obj.capital}
+                <strong>Capital:</strong> {capital.length > 0 ? capital : "N/A"}
             </p>
         </li>
     );
