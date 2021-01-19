8567c72aa3e187d332a8efd8fc7ddd513c468efb
cagloria <33134245+cagloria@users.noreply.github.com>
2021-01-18 19:57:49 -0700

    Updates

---
+++ precache-manifest.c7266d8db996371e5301d82653523cae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2391a9936f6aa5e2b5daf366761fddf1",
+    "url": "/rest-countries-api/index.html"
+  },
+  {
+    "revision": "c86050c8c9c8a050a8ac",
+    "url": "/rest-countries-api/static/css/main.a4fab22e.chunk.css"
+  },
+  {
+    "revision": "9365e592e30b11c2d86b",
+    "url": "/rest-countries-api/static/js/2.070c2f37.chunk.js"
+  },
+  {
+    "revision": "ffa35a34964860fdc97ed79ca9abd27c",
+    "url": "/rest-countries-api/static/js/2.070c2f37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c86050c8c9c8a050a8ac",
+    "url": "/rest-countries-api/static/js/main.0656829d.chunk.js"
+  },
+  {
+    "revision": "8dcae019207c17fca135",
+    "url": "/rest-countries-api/static/js/runtime-main.ecc88822.js"
+  }
+]);
